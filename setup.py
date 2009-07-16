322c6a9b16c4f5b578207f8dc6a4dabb19feff96
Fabio Corneti <info@corneti.com>
2009-07-16 18:42:17 +0200

    Setuptools compatibility

--- setup.py
+++ setup.py
@@ -1,12 +1,21 @@
-from distutils.core import setup
- 
+from setuptools import setup
+from setuptools import find_packages
+
+version = '3.1'
+
 setup(
-    name = "sorl",
-    version = "3.1",
+    name='sorl',
+    version=version,
+    description="",
+    url='http://code.google.com/p/sorl-thumbnail/',
     packages = [
         "sorl",
         "sorl.thumbnail",
         "sorl.thumbnail.templatetags",
         "sorl.thumbnail.tests",
+    ],
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
     ],
 )
